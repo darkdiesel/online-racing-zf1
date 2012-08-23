7a9c3b9a9d667069ccaf1c05816e830e0c654fc7
Igor Peshkov <igor.peshkov@gmail.com>
2012-08-23 21:27:31 +0300

    Add docs for database in Microsoft Access format. Create Zend Application.

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,23 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+	protected function _initView()
+    {
+        // ������������� ����
+        $view = new Zend_View();
+        $view->doctype('XHTML1_STRICT');
+        $view->headTitle('Online-Racing');
+        $view->headLink()->appendStylesheet($view->baseUrl("css/style.css"));
+        $view->headScript()->appendFile($view->baseUrl("scripts/jquery-1.8.0_min.js"));
 
+        // ���������� ���� � ViewRenderer
+        $viewRenderer = Zend_Controller_Action_HelperBroker::getStaticHelper(
+            'ViewRenderer'
+        );
+        $viewRenderer->setView($view);
+ 
+        // ��� �����������, ����� �������, �� ����� ���� �������� �����������
+        return $view;
+    }
 
 }
-
