92d5edd420be1070792fa3122337073ab7cc2aa1
Igor Peshkov <igor.peshkov@gmail.com>
2012-08-27 00:27:31 +0300

    Delete some files.

--- bin/startmigration.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set /p ConfigName= Please enter name of configuration:
-php app.php update database-schema -e %ConfigName%
-pause
--- bin/startmigration.sh
+++
@@ -1,2 +0,0 @@
-#/bin/bash
-php app.php update database-schema -e $1
