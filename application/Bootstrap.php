408ba1e2c0578158eee1511028725bd9179c918b
Igor Peshkov <igor.peshkov@gmail.com>
2012-08-29 18:29:35 +0300

    Add slider to template and to production. Fix some bugs

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,28 +1,39 @@
-<?php
+﻿<?php
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 	protected function _initView()
     {
-        // ������������� ����
+        // ????????????? ????
         $view = new Zend_View();
 
         $view->headTitle('Online-Racing');
         $view->headLink()->appendStylesheet($view->baseUrl("css/bootstrap.min.css"));
         $view->headLink()->appendStylesheet($view->baseUrl("css/style.css"));
         $view->headLink()->appendStylesheet($view->baseUrl("css/admin_menu.css"));
-        $view->headScript()->appendFile($view->baseUrl("js/jquery-1.8.0_min.js"));
+        
+		// start slider css
+		$view->headLink()->appendStylesheet($view->baseUrl("css/skitter.styles.css"));
+		// end slider css
+		
+		$view->headScript()->appendFile($view->baseUrl("js/jquery-1.8.0.min.js"));
         $view->headScript()->appendFile($view->baseUrl("js/bootstrap.min.js"));
         $view->headScript()->appendFile($view->baseUrl("js/jquery.lavalamp.js"));
+		
+		// start slider js
+		$view->headScript()->appendFile($view->baseUrl("js/jquery.easing.1.3.js"));
+		$view->headScript()->appendFile($view->baseUrl("js/jquery.animate_colors.min.js"));
+		$view->headScript()->appendFile($view->baseUrl("js/jquery.skitter.my.js"));
         $view->headScript()->appendFile($view->baseUrl("js/my_js.js"));
-
-        // ���������� ���� � ViewRenderer
+		// end slider js
+		
+        // ?????????? ???? ? ViewRenderer
         $viewRenderer = Zend_Controller_Action_HelperBroker::getStaticHelper(
             'ViewRenderer'
         );
         $viewRenderer->setView($view);
  
-        // ��� �����������, ����� �������, �� ����� ���� �������� �����������
+        // ??? ???????????, ????? ???????, ?? ????? ???? ???????? ???????????
         return $view;
     }
 
