5f8bd75ed94796b2f691d64454a93cb812c10699
Igor Peshkov <igor.peshkov@gmail.com>
2012-11-19 18:13:51 +0300

    Change view of last added. Signed-off-by:Igor Peshkov <igor.peshkov@gmail.com>

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -16,10 +16,6 @@ class IndexController extends Zend_Controller_Action
 		$this->view->headScript()->appendFile($this->view->baseUrl("js/jquery.skitter.js"));
                 $this->view->headLink()->appendStylesheet($this->view->baseUrl("css/skitter.styles.css"));
 
-		// Css for skitter slider
-                $this->view->headScript()->appendFile($this->view->baseUrl("js/jquery.jcarousel.min.js"));
-                $this->view->headLink()->appendStylesheet($this->view->baseUrl("css/carousel.css"));
-
         // page title
         $this->view->headTitle($this->view->translate('Портал Онлай Скорости'));
     }
