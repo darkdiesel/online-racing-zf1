4c39d26921445529ea4bcf2b6f754b2eedd748c2
Igor Peshkov <igor.peshkov@gmail.com>
2013-10-20 02:56:02 +0300

    Fix bugs with page scroller for webkit browsers.

--- public_html/js/page_scroller.js
+++ public_html/js/page_scroller.js
@@ -1,13 +1,14 @@
 jQuery(document).ready(function($) {
     $("#page-scroller").on("click", "#topcontrol", function(e){
-	$('html').animate({
+	$('html,body').animate({
 	    scrollTop:0
 	}, 800);
 	e.preventDefault();
     }).on("click", "#downcontrol", function(e){
-	$('html').animate({
+	$('html,body').animate({
 	    scrollTop: $(document).outerHeight()
 	}, 800);
+        
 	e.preventDefault();
     });
 });
