d96372e2b0584a55439b724270a811d0d8619d47
Igor Peshkov <igor.peshkov@gmail.com>
2014-01-09 15:07:20 +0300

    Add integration CKFinder for CKEditor.

--- library/App/View/Helper/CKEditor.php
+++ library/App/View/Helper/CKEditor.php
@@ -24,6 +24,53 @@ class App_View_Helper_CKEditor extends Zend_View_Helper_Abstract {
 				break;
 		}
 	}
+	
+	public function setupBBCodeEditor($textareaId) {
+        return "
+                CKEDITOR.replace('{$textareaId}', {
+                    extraPlugins : 'bbcode,font,smiley,find,selectall,colorbutton,wordcount',
+		removePlugins: 'bidi,dialogadvtab,div,filebrowser,flash,forms,horizontalrule,iframe,justify,liststyle,pagebreak,showborders,stylescombo,table,tabletools,templates',
+        // Width and height are not supported in the BBCode format, so object resizing is disabled.
+        disableObjectResizing: true,
+        // Define font sizes in percent values.
+        fontSize_sizes: \"30/30%;50/50%;100/100%;120/120%;150/150%;200/200%;300/300%\",
+        toolbar: [
+          [ 'Source' ],
+          [ 'Save', 'NewPage', '-', 'Undo', 'Redo' ],
+          [ 'Find', 'Replace', '-', 'SelectAll' ],
+          [ 'Link', 'Unlink', 'Image', 'Smiley', 'SpecialChar' ],
+          '/',
+          [ 'Bold', 'Italic', 'Underline', '-', 'RemoveFormat' ],
+          [ 'FontSize' ],
+          [ 'TextColor' ],
+          [ 'NumberedList', 'BulletedList', '-', 'Blockquote' ],
+          [ 'Maximize' ],
+          [ 'About' ]
+        ],
+                    });
+                ";
+    }
+	
+	public function SetupFullHtmlEditor($textareaId) {
+        return "CKEDITOR.replace('" . $textareaId . "', {
+            extraPlugins : 'iframe,iframedialog,font,smiley,find,selectall,colorbutton,wordcount,oembed',
+            disableObjectResizing: true,
+            fontSize_sizes: \"30/30%;50/50%;100/100%;120/120%;150/150%;200/200%;300/300%\",
+            toolbar: [
+              [ 'Source' ],
+              [ 'Save', 'NewPage', '-', 'Undo', 'Redo' ],
+              [ 'Find', 'Replace', '-', 'SelectAll' ],
+              [ 'Link', 'Unlink', 'Image', 'Smiley', 'SpecialChar', 'oembed' ],
+              '/',
+              [ 'Bold', 'Italic', 'Underline', '-', 'RemoveFormat' ],
+              [ 'Font', 'FontSize' ],
+              [ 'TextColor' ],
+              [ 'NumberedList', 'BulletedList', '-', 'Blockquote' ],
+              [ 'Maximize' ],
+              [ 'About' ]
+            ]
+        });";
+    }
 
 	private function bbcode($id) {
 		$configs = "";
--- library/App/View/Helper/CKFinder.php
+++ library/App/View/Helper/CKFinder.php
@@ -10,5 +10,12 @@ class App_View_Helper_CKFinder extends Zend_View_Helper_Abstract {
 		$this->view->headScript()->appendFile($this->view->baseUrl("library/ckfinder/ckfinder.js"));
 		//return $this;
 	}
+	
+	public function setupCKEditor($editor){
+		return "var ck_editor_finder = CKEDITOR.instances['{$editor}'];"
+		. "if (ck_editor_finder) {"
+		. "CKFinder.setupCKEditor(ck_editor_finder, '/library/ckfinder/' );"
+		."}";
+	}
 
 }
