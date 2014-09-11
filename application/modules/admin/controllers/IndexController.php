d8de9db0750c21bd972fc33956d648be112a1cd5
Igor Peshkov <Igor.Peshkov@gmail.com>
2014-09-11 18:37:52 +0300

    Update admin layout-admin.

--- application/modules/admin/controllers/IndexController.php
+++ application/modules/admin/controllers/IndexController.php
@@ -1,14 +1,18 @@
 <?php
 
-class Admin_IndexController extends App_Controller_LoaderController {
+class Admin_IndexController extends App_Controller_LoaderController
+{
 
-	public function init() {
-		parent::init();
-		/* Initialize action controller here */
-	}
+    public function init()
+    {
+        parent::init();
+        /* Initialize action controller here */
+    }
 
-	public function indexAction() {
-		$this->view->headTitle($this->view->translate('Главная'));
-	}
+    public function indexAction()
+    {
+        $this->view->headTitle($this->view->translate('Панель управления'));
+        $this->view->pageTitle($this->view->translate('Панель управления'));
+    }
 
 }
