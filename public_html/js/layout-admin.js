bbdc47e64ca4c8dfdf6602b86ed5f4e02eeaed68
Igor Peshkov <Igor.Peshkov@gmail.com>
2014-09-12 18:34:09 +0300

    Update admin layout.

--- public_html/js/layout-admin.js
+++ public_html/js/layout-admin.js
@@ -1,14 +1,14 @@
 (function ($) {
-    $(document).ready(function(){
+    $(document).ready(function () {
         resizeSidebar();
-        $(window).on('resize', function(el){
+        $(window).on('resize', function (el) {
             resizeSidebar();
         });
 
-        function resizeSidebar(){
-            $('#left-sidebar').height($(document).height()-$("#header").outerHeight());
-            console.log(1);
+        function resizeSidebar() {
+            $('#left-sidebar').height($(document).height() - $("#header").outerHeight());
         }
 
+        $('#digital-clock').clock({ type: 'digital'});
     });
 })(jQuery);
